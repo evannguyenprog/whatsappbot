84abc0e75b4e7caccf6a4f6928bb7b5da9d18d2e
Evan Nguyen <48500302+evannguyenprog@users.noreply.github.com>
2022-07-22 16:14:44 -0400

    add chromedriver

--- messenger.py
+++ messenger.py
@@ -1,6 +1,10 @@
 import os
 from whatsapp import WhatsApp
 
+from whatsapp import WhatsApp
+whatsapp = WhatsApp(10)
+print(whatsapp.send_message("Name",":heart: Good!"))  
+
 whatsapp = WhatsApp(100, session="mysession")
 whatsapp.send_document("Evan", os.path.join(os.getcwd(), "message.txt"))
 
